007f5ebd2dfe8dfe5f7769685f1c8255e91d405b
Athéna <ytblackfalcon@gmail.com>
2025-10-15 23:38:24 +0200

    Fix Docker build & start
    Updates the Docker configuration to streamline the deployment process for the Voyagers of Nera dedicated server.
    
    Updates the server update logic to update only if older than 14 days.
    
    Migrates to a steamcmd-based image for simpler server setup.
    
    Also, adjusts image name and labels in the docker-compose configuration for clarity.
    
    Still need to publish the image & test the docker compose

--- start.sh
+++ start.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-set -e
-
-# Définition du répertoire d'installation
+UPDATE_STAMP="/voyagers_server/.last_update"
 INSTALL_DIR="/voyagers_server/BoatGame/Server"
+APP_ID=3937860
+LOG_FILE="/voyagers_server/wine.log"
 
-# Téléchargement des fichiers du serveur si nécessaire
-if [ ! -d "$INSTALL_DIR" ]; then
-    echo "Téléchargement des fichiers du serveur..."
-    /steamcmd/steamcmd.sh +login anonymous +force_install_dir $INSTALL_DIR +app_update 3937860 validate +quit
+# Vérifie si update.sh doit être exécuté (plus de 14 jours)
+if [ ! -f "$UPDATE_STAMP" ] || [ $(( ( $(date +%s) - $(stat -c %Y "$UPDATE_STAMP") ) / 86400 )) -ge 14 ]; then
+    echo "Mise à jour du serveur..."
+    steamcmd +force_install_dir $INSTALL_DIR +login anonymous +app_update $APP_ID validate +quit
+    touch "$UPDATE_STAMP"
 fi
 
 # Lancement du serveur avec Wine
 echo "Démarrage du serveur..."
-wine64 $INSTALL_DIR/BoatGame/Binaries/Win64/BoatGameServer-Win64-Shipping_BE.exe "$@"
+ls $INSTALL_DIR/BoatGame/Binaries/Win64/
+xvfb-run wine $INSTALL_DIR/BoatGame/Binaries/Win64/BoatGameServer-Win64-Shipping.exe "$@"
